863141c731de1b6e120f83e63746397a9715659a
Swapnil <swapnilsrivastava2002@gmail.com>
2025-07-08 23:51:53 +0530

    Readme file added

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -23,7 +23,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main
+     + <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
           isSidebarCollapsed ? "" : "md:pl-64"
         }`}
